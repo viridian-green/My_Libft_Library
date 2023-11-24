5acfe0c5f346494dcce8cf0c369ccc5c9273c86d
Adele Martin <ademarti@c4a10c8.42berlin.de>
2023-11-24 16:52:05 +0100

    ok

--- libt_01/ft_strrchr.c
+++ libt_01/ft_strrchr.c
@@ -6,30 +6,36 @@
 /*   By: ademarti <ademarti@student.42berlin.de     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 13:49:54 by ademarti          #+#    #+#             */
-/*   Updated: 2023/11/22 16:27:42 by ademarti         ###   ########.fr       */
+/*   Updated: 2023/11/24 16:51:55 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <bsd/string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 
 char	*ft_strrchr(const char *s, int c)
 {
-	size_t			i;
-	unsigned char	*last_occurrence;
+	unsigned int		i;
+	char	*last_occurrence;
+	char cc;
 
-	last_occurrence = NULL;
 	i = 0;
+	cc = (char)c;
+	last_occurrence = NULL;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == cc)
 		{
-			last_occurrence = (unsigned char *)s + i;
+			last_occurrence = (char *)&s[i];
 		}
 		i++;
 	}
-	if (last_occurrence == NULL)
-	{
-		return (NULL);
-	}
-	return ((char *)last_occurrence);
+	if (s[i] == cc)
+		last_occurrence = (char *)&s[i];
+	return (last_occurrence);
 }
