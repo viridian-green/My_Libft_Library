3d9c74d7086bb91fe5eaccb6f8829bbf566d4221
moldypeaches <adelejulietmartin@gmail.com>
2023-11-24 15:14:47 +0100

    ok

---
+++ libt_01/ft_itoa.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+int	size_of_int(int n)
+{
+
+}
+
+char	*ft_itoa(int n)
+{
+	char	*result;
+
+	result = (char *)malloc(sizeof(char) * )
+	if (n > 10)
+	{
+
+	}
+
+
+
+
+	return ()
+
+}
