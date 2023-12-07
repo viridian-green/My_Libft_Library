fc3f2f95e994a7bdc3e61dfc2845dd6538cf9d65
moldypeaches <adelejulietmartin@gmail.com>
2023-12-07 14:45:50 +0100

    everything ok

--- libt_01/ft_memmove.c
+++ libt_01/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ademarti <adelemartin@student.42.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 12:19:38 by ademarti          #+#    #+#             */
-/*   Updated: 2023/12/05 15:16:47 by ademarti         ###   ########.fr       */
+/*   Updated: 2023/12/07 13:15:28 by ademarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,19 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char	*p_dest;
 	char	*p_src;
-	size_t	i;
 
 	p_dest = (char *)dest;
 	p_src = (char *)src;
-	i = 0;
 	if (!dest && !src)
 		return (NULL);
-	if (dest == src)
-		return (dest);
-	if (p_dest < p_src)
+	if (p_dest > p_src)
 	{
-		while (i < n)
+		while (n--)
 		{
-			p_dest[i] = p_src[i];
-			i++;
-		}
-	}
-	else
-		while (n > 0)
-		{
-			n--;
 			p_dest[n] = p_src[n];
 		}
+	}
+	else if (p_dest < p_src)
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
